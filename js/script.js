f52eb5126842bf2d2fcfddfe21c2d5abc40ed7f7
julienle17 <julienleclercq17@gmail.com>
2019-04-11 16:27:21 +0200

    add class in html & create variable in js & add class .score in css

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let scorePlayer1 = document.getElementsByClassName('scorePlayer1');
+let scorePlayer2 = document.getElementsByClassName('scorePlayer2');
+let buttonPlayer1 = document.getElementsByClassName('buttonPlayer1');
+let buttonPlayer2 = document.getElementsByClassName('buttonPlayer2');
+let buttonReset = document.getElementsByClassName('buttonReset');
+let scoreLimitInput = document.getElementsByClassName('').value;
+
