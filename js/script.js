a376d1f5c14895cbb580cc8527597b06fda89317
julienle17 <julienleclercq17@gmail.com>
2019-04-14 10:49:42 +0200

    add class on increment all player & remove class in reset button

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ let scorePlayer2 = document.getElementsByClassName('scoreplayer2')[0].innerHTML;
 const buttonPlayer1 = document.getElementsByClassName('buttonplayer1')[0];
 const buttonPlayer2 = document.getElementsByClassName('buttonplayer2')[0];
 const buttonReset = document.getElementsByClassName('buttonreset');
-//let scoreLimitInput = document.getElementsByClassName('scorelimitinput').value = (5);
 let scoreLimit = 5;
 
 function incrementScorePlayer1() {
 		scorePlayer1++;
 		document.getElementsByClassName('scoreplayer1')[0].innerHTML = scorePlayer1;
 	if (scorePlayer1 == scoreLimit) {
+			scorePlayer1.className += 'player1winner';
+			scorePlayer2.className += 'player2looser';
 			freezePlayerButton();
 			return;
 	} 
@@ -21,6 +22,8 @@ function incrementScorePlayer2() {
 		scorePlayer2++;
 		document.getElementsByClassName('scoreplayer2')[0].innerHTML = scorePlayer2;
 	if (scorePlayer2 == scoreLimit) {
+			scorePlayer2.className += 'player2winner';
+			scorePlayer1.className += 'player1looser';
 			freezePlayerButton();
 			return;
 	} 
@@ -34,8 +37,12 @@ function freezePlayerButton() {
 function resetScoreDisplay() {
 	scorePlayer1 = 0;
 	document.getElementsByClassName('scoreplayer1')[0].innerHTML = 0;
+	scorePlayer1.className.remove('player1winner';'player1losser');
 	scorePlayer2 = 0;
 	document.getElementsByClassName('scoreplayer2')[0].innerHTML = 0;
+	scorePlayer2.className.remove('player2winner';'player2losser');
 	buttonPlayer1.disabled = false;
 	buttonPlayer2.disabled = false;
 }
+
+
